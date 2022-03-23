dc5d57bd24029f65824155d2174f649ae0806053
Anurag Hakeem <anuraghakeem@gmail.com>
2022-03-23 20:08:48 +0530

    minor text update

--- cryptomovietv/components/PopularMovie.tsx
+++ cryptomovietv/components/PopularMovie.tsx
@@ -22,7 +22,7 @@ interface MOVIESCOMP{
 const PopularMovie = ({ movies }:MOVIESCOMP) => {
     return (
       <div className="container max-w-7x1 mx-auto pb-10 px-4 font-lato" id='popular-movie'>
-        <h1 className="text-primary mt-8 mb-9 text-4xl font-semibold text-center">❤<br />What Our Community Loves</h1>
+        <h1 className="text-primary mt-8 mb-9 text-4xl font-semibold text-center">❤<br />Community Recommendation</h1>
         <div className="grid gap-2 grid-cols-2 sm:grid-cols-2 sm:gap-9 md:grid-cols-3 md:gap-9 lg:gap-9 lg:grid-cols-5">
         {movies.map((movie) => <MovieCard movie={movie} key={movie.id} />)}
         </div>
